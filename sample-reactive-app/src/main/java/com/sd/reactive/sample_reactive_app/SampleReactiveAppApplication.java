9fde8f73c97d4405a6f3f65e4cf76430fab46a9d
shubhajit <shubhajitdas121@gmail.com>
2024-12-31 21:26:03 +0530

    initial reactive sample project

---
+++ sample-reactive-app/src/main/java/com/sd/reactive/sample_reactive_app/SampleReactiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.sd.reactive.sample_reactive_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleReactiveAppApplication.class, args);
+	}
+
+}
---
+++ sample-reactive-app/src/test/java/com/sd/reactive/sample_reactive_app/SampleReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.reactive.sample_reactive_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleReactiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
